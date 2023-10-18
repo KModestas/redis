fbc82baf7b479c34e139ca795ebfc121307273c7
Modestas <modestask97@gmail.com>
2023-10-18 15:35:37 +0100

    enforce unique username with sets

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,9 +1,9 @@
 // to avoid magic strings, we create utils to get each redis key.
 // # denotes the id of the entity. This ensures we dont need to store it as part of the hash which saves memory. 
-
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
+export const usernamesUniqueKey = () => 'usernames:unique';
 
 // Items
 export const itemsKey = (itemId: string) => `items#${itemId}`;
