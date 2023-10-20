457640020f7597d92c0f2862fc36e392ed36d9a3
Modestas <modestask97@gmail.com>
2023-10-20 12:21:37 +0100

    createdBid with watch + transaction

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,10 +1,46 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { client } from '$services/redis';
-import { bidHistoryKey } from '$services/keys';
+import { bidHistoryKey, itemsKey, itemsByPriceKey } from '$services/keys';
+import { getItem } from './items';
 
 import { DateTime } from 'luxon';
 
-export const createBid = async (attrs: CreateBidAttrs) => { };
+export const createBid = async (attrs: CreateBidAttrs) => {
+	// execute our transaction in a new redis connection
+	return client.executeIsolated(async (isolatedClient) => {
+		// watch for changes to the current item that we are trying to update. Cancel the transaction if a change from elseware occurs (prevent concurrency issues)
+		await isolatedClient.watch(itemsKey(attrs.itemId));
+
+		const item = await getItem(attrs.itemId);
+
+		if (!item) {
+			throw new Error('Item does not exist');
+		}
+		if (item.price >= attrs.amount) {
+			throw new Error('Bid too low');
+		}
+		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+			throw new Error('Item closed to bidding');
+		}
+
+		const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+		// execute rPush, hSet zAdd commands as part of a transaction
+		return isolatedClient
+			.multi()
+			.rPush(bidHistoryKey(attrs.itemId), serialized)
+			.hSet(itemsKey(item.id), {
+				bids: item.bids + 1,
+				price: attrs.amount,
+				highestBidUserId: attrs.userId
+			})
+			.zAdd(itemsByPriceKey(), {
+				value: item.id,
+				score: attrs.amount
+			})
+			.exec();
+	});
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
 	// since we can only retrieve values from a list via index, use offset and count to calculate index:
