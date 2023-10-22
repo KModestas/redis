8b0080b9bf4e2e920952bb53af124cbd8565fbe0
Modestas <modestask97@gmail.com>
2023-10-22 11:22:31 +0100

    create indexes for searching

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,6 +1,7 @@
 import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 import { createClient, defineScript } from 'redis';
+import { createIndexes } from './create-indexes';
 
 
 const client = createClient({
@@ -65,5 +66,13 @@ const client = createClient({
 client.on('error', (err) => console.error(err));
 
 client.connect();
+
+client.on('connect', async () => {
+	try {
+		await createIndexes();
+	} catch (err) {
+		console.error(err);
+	}
+});
 
 export { client };
