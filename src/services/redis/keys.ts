04dd5145f7eb5584e64f32836e93d846e8b6607f
Modestas <modestask97@gmail.com>
2023-10-24 17:17:12 +0100

    comment what data type each key is using

--- src/services/redis/keys.ts
+++ src/services/redis/keys.ts
@@ -1,18 +1,20 @@
-// to avoid magic strings, we create utils to get each redis key.
-// # denotes the id of the entity. This ensures we dont need to store it as part of the hash which saves memory. 
-export const pageCacheKey = (id: string) => `pagecache#${id}`;
-export const usersKey = (userId: string) => `users#${userId}`;
-export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
-export const usernamesUniqueKey = () => 'usernames:unique';
-export const userLikesKey = (userId: string) => `users:likes#${userId}`
-export const usernamesKey = () => 'usernames';
+// to avoid typos, we create utils to generate each redis key.
+// : denotes the entity e.g. users
+// # denotes the individual id of each entity. This ensures we dont need to store it as part of the hash (saves memory) 
 
+// comments on the right denote datatype being stored under each key
+export const pageCacheKey = (id: string) => `pagecache#${id}`; // STRING
+export const usersKey = (userId: string) => `users#${userId}`; // HASH
+export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`; // HASH
+export const usernamesUniqueKey = () => 'usernames:unique'; // SET
+export const userLikesKey = (userId: string) => `users:likes#${userId}` // SET
+export const usernamesKey = () => 'usernames'; // SORTED SET (ZSET)
 
-// Items
-export const itemsKey = (itemId: string) => `items#${itemId}`;
-export const itemsByViewsKey = () => 'items:views';
-export const itemsByEndingAtKey = () => 'items:endingAt';
-export const itemsViewsKey = (itemId: string) => `items:views#${itemId}`;
-export const itemsByPriceKey = () => 'items:price';
-export const bidHistoryKey = (itemId: string) => `history#${itemId}`;
-export const itemsIndexKey = () => 'idx:items';
+// Items                                                                          
+export const itemsKey = (itemId: string) => `items#${itemId}`; // HASH
+export const bidHistoryKey = (itemId: string) => `history#${itemId}`; // LIST
+export const itemsViewsKey = (itemId: string) => `items:views#${itemId}`; // HYPERLOGLOG 
+export const itemsByViewsKey = () => 'items:views'; // SORTED SET (ZSET)
+export const itemsByEndingAtKey = () => 'items:endingAt'; // SORTED SET (ZSET)
+export const itemsByPriceKey = () => 'items:price'; // SORTED SET (ZSET)
+export const itemsIndexKey = () => 'idx:items'; // INDEX
